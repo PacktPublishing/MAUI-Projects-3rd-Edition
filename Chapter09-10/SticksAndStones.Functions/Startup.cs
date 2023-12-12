borrillis/NET-MAUI-Projects

Creating the Connect Models

Chapter11/SticksAndStones.Functions/Startup.cs
@@ -1,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SticksAndStones.Repository;
+
+[assembly: FunctionsStartup(typeof(SticksAndStones.Startup))]
+
+namespace SticksAndStones;
+
+internal class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddDbContext<GameDbContext>(
+            options =>
+            {
+                options.UseInMemoryDatabase("SticksAndStones");
+            });
+    }
+}

PacktPublishing/MAUI-Projects-3rd-Edition

Chapter09-10/SticksAndStones.Functions/Startup.cs
@@ -1,0 +1,20 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using SticksAndStones.Repository;
+
+[assembly: FunctionsStartup(typeof(SticksAndStones.Startup))]
+
+namespace SticksAndStones;
+
+internal class Startup : FunctionsStartup
+{
+    public override void Configure(IFunctionsHostBuilder builder)
+    {
+        builder.Services.AddDbContextFactory<GameDbContext>(
+            options =>
+            {
+                options.UseInMemoryDatabase("SticksAndStones");
+            });
+    }
+}